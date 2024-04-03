c9270e33cc79a9dd065d6d330e46949e69a9c611
hannamagi <hanna.magi.001@voco.ee>
2024-04-03 15:31:21 +0300

    Added data to component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+    const expenses = [
+        {
+            date: new Date(2023, 0, 10),
+            title: "New book",
+            price: 30.99
+        },
+        {
+            date: new Date(2023, 0, 10),
+            title: "Hype Boy By Newjeans",
+            price: 99.99
+        }
+    ]
     return (
         <div className="App">
-            <ExpenseItem></ExpenseItem>
-            <ExpenseItem></ExpenseItem>
+            <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
+            <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
         </div>
     );
 }
