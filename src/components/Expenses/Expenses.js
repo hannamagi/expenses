da6c667091df46a00fb49a5b9390dfb6348f8f35
hannamagi <hanna.magi.001@voco.ee>
2024-04-03 16:02:26 +0300

    Andmete suunamine (bottom-up) ja väärtuse hetkeseisu lugemine ning määramine

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
 import Card from "../UI/Card";
+import ExpensesFilter from './ExpensesFilter';
 
-function Expenses(props){
+function Expenses(props) {
+    const ExpenseFilterHandler = (selectedYear) => {
+        console.log(selectedYear)
+    }
     return (
-        <Card className="expenses">
-            <ExpenseItem expenseData={props.expenseData[0]}></ExpenseItem>
-            <ExpenseItem expenseData={props.expenseData[1]}></ExpenseItem>
+        <Card className={'expenses'}>
+            <ExpensesFilter onExpenseFilter={ExpenseFilterHandler}/>
+            <ExpenseItem expenseData={props.expenseData[0]}/>
+            <ExpenseItem expenseData={props.expenseData[1]}/>
         </Card>
-    )
+    );
 }
-
 export default Expenses;
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ExpensesFilter.css';
+
+const ExpensesFilter = () => {
+    const filterChangeHandler = (event) => {
+        console.log(event.target.value)
+    }
+    return (
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+                <label>Filter by year</label>
+                <select onChange={filterChangeHandler}>
+                    <option value='2023'>2023</option>
+                    <option value='2024'>2024</option>
+                    <option value='2025'>2025</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
