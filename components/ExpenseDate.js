bc8dbc4a535fed0e7fa450e959ed23d851ecb5f0
hannamagi <hanna.magi.001@voco.ee>
2024-04-03 15:37:18 +0300

    Adde css to date

--- components/ExpenseDate.js
+++ components/ExpenseDate.js
@@ -4,11 +4,11 @@ function ExpenseDate(props){
     const year = props.date.getFullYear();
 
     return (
-        <div className="expense-item">
+        <div className="expense-date">
             <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
+                <div className="expense-date__month">{month}</div>
+                <div className="expense-date__day">{day}</div>
+                <div className="expense-date__year">{year}</div>
             </div>
         </div>
     )
